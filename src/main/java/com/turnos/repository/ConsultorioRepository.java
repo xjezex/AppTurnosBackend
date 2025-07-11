4b0ba5de6e18d82c1172c369ce7a1618b1af6940
ecasada <ezequiel.casada2@globallogic.com>
2025-07-10 22:40:14 -0300

    add: TurnoRepository

---
+++ src/main/java/com/turnos/repository/ConsultorioRepository.java
@@ -0,0 +1,8 @@
+package com.turnos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConsultorioRepository extends JpaRepository {
+}
